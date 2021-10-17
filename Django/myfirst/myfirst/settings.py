69a399fe4bb4fcfe80209673960c5a64a5385f51
DimitriiTrater <52676168+DimitriiTrater@users.noreply.github.com>
2021-10-17 21:52:53 +0500

    Update settings.py

--- Django/myfirst/myfirst/settings.py
+++ Django/myfirst/myfirst/settings.py
@@ -22,7 +22,8 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-)n=cf2k9myktl4dp^5n+)#pnvqyr&b9iif^q&3hlf(#5g(s*y^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
